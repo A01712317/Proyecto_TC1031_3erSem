b3417794de01d051adb2d4c5d89b87323221fe74
A01712317 <149627608+A01712317@users.noreply.github.com>
2024-09-29 10:38:36 -0600

    Update piloto.h

--- Proyecto/piloto.h
+++ Proyecto/piloto.h
@@ -14,9 +14,15 @@ private:
     int posicion;
 
 public:
-    Piloto(string n, int num, double t1, double t2, double t3) 
-        : Tiempo(t1, t2, t3), nombre(n), numero(num), posicion(0) {}
+    // Default constructor
+    Piloto() : Piloto("", 0, 0.0, 0.0, 0.0) {}  
 
+    // Parameterized constructor
+    Piloto(string n, int num, double t1, double t2, double t3)
+        : Tiempo(t1, t2, t3),  // Initialize the base class Tiempo
+          nombre(n), numero(num), posicion(0) {}
+
+    // Getters and Setters
     string getNombre() const { return nombre; }
     int getNumero() const { return numero; }
     double getTiempoTotal() const { return getTiempo(); }
